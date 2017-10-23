6a07c2433abc0d333304d5587acad93106e6fafa
Arhitektor76 <31167169+Arhitektor76@users.noreply.github.com>
2017-10-23 23:05:16 +0300

    1

--- WpWeek3/WpWeek3/MainWindow.xaml.cs
+++ WpWeek3/WpWeek3/MainWindow.xaml.cs
@@ -26,5 +26,31 @@ namespace WpWeek3
         {
             InitializeComponent();
         }
+
+        private void MenuItem_Click_add(object sender, RoutedEventArgs e)
+        {
+            Tuning tuning = new Tuning();
+            tuning.ShowDialog();
+        }
+
+        private void MenuItem_Click_delete(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_update(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_clear(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_exit(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
